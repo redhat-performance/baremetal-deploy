b037b830bf5524a72f693e046236b58a47829c6c
jaredoconnell <joconnel@redhat.com>
2020-06-30 14:55:33 -0400

    Add easy shell script to build container locally

--- run-jetski.sh
+++ run-jetski.sh
@@ -2,6 +2,6 @@ ansible_dir=/root/JetSki/ansible-ipi-install
 
 podman run -it \
 	-v ./ansible-ipi-install/group_vars/all.yml:$ansible_dir/group_vars/all.yml:Z \
-	-t localhost/with-centos
+	-t localhost/jetski
 	#/bin/bash
 	#ansible-playbook -vvv -i $ansible_dir/inventory/jetski/hosts $ansible_dir/playbook-jetski.yml
