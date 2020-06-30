bb25e31669802f7680552039ef27b44d951e61e0
jaredoconnell <joconnel@redhat.com>
2020-06-30 14:57:01 -0400

    Remove unnecessary comments

--- run-jetski.sh
+++ run-jetski.sh
@@ -3,5 +3,3 @@ ansible_dir=/root/JetSki/ansible-ipi-install
 podman run -it \
 	-v ./ansible-ipi-install/group_vars/all.yml:$ansible_dir/group_vars/all.yml:Z \
 	-t localhost/jetski
-	#/bin/bash
-	#ansible-playbook -vvv -i $ansible_dir/inventory/jetski/hosts $ansible_dir/playbook-jetski.yml
